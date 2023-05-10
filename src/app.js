c3df663c5ffcb88d4121427b6c73bdf68eb0f95c
Tin Nguyen <anglekiller9x@gmail.com>
2023-05-10 16:37:55 +0700

    evironment

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
-
-const express = require('express')
+import express from "express";
+//const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
+})
 app.get('/', (req, res) => {
     res.send('Hello Worlds!')
 })
